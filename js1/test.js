23f4899d828c3907f91d0a5c7a39971deb5cd398
ronok420 <82023374+ronok420@users.noreply.github.com>
2024-01-12 09:37:11 +0000

    first commit

---
+++ js1/test.js
@@ -0,0 +1,9 @@
+console.log("ronok")
+const userId = 1234;
+let  userEmail = "user@gmail.com";
+var userAddress = "solipara";
+   userCity ="jaipur";
+   userCity="meherpur"
+   userEmail="nobilla@gmail.com"
+
+   console.table([userId, userEmail,userAddress,userCity]);
