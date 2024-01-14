508afa4708b09fc65f0342412baeb78674a3f42b
ronok420 <82023374+ronok420@users.noreply.github.com>
2024-01-14 10:08:12 +0000

    up to qs3

--- js1/string.js
+++ js1/string.js
@@ -14,6 +14,38 @@ function is_string(input) {
   }
   
   // Test cases
+  console.log('qs:1')
   console.log(is_string('w3resource'));  // true
   console.log(is_string([1, 2, 4, 0]));  // false
-  
+
+
+// qs2:  Write a JavaScript function to check whether a string is blank or not.
+
+// Test Data :
+// console.log(is_Blank(''));
+// console.log(is_Blank('abc'));
+// true
+// false
+function is_Blank(input){
+    if(input.length === 0)
+      return "string is blank";
+       
+      return "string has character";
+    
+    }
+    console.log('qs:2')
+    console.log(is_Blank(''));
+    console.log(is_Blank('abc'));
+   
+   
+   
+   
+//   qs:3  Write a JavaScript function to split a string and convert it into an array of words.
+
+// Test Data :
+// console.log(string_to_array("Robin Singh"));
+// ["Robin", "Singh"]
+const string_to_array = (input)=> input.trim().split(" ")
+
+console.log("qs:3")
+console.log(string_to_array("Robin Singh"));
