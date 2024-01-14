a831f65ec1a2ceb146c8a34981ebd879cb58dd4a
ronok420 <82023374+ronok420@users.noreply.github.com>
2024-01-14 11:38:29 +0000

    qs5

--- js1/string.js
+++ js1/string.js
@@ -64,3 +64,17 @@ function extract_characters(inputString, numCharacters) {
 console.log('qs:4')
 console.log(extract_characters("Hello, World!", 5));
 // Output: "Hello"
+
+
+//qs:5 Write a JavaScript function to convert a string into abbreviated form.
+abbrev_name = function (str1) {
+  var split_names = str1.trim().split(" ");
+  if (split_names.length > 1) {
+      return (split_names[0] + " " + split_names[1].charAt(0) + ".");
+  }
+  return split_names[0];
+};
+console.log("qs:5");
+console.log(abbrev_name("Robin Singh"));
+const sname ="Robin Singh".trim().split(" ")
+console.log(`${sname[0]} ${sname[1].charAt(0)}.`)
